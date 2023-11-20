28e151030eeb185739d793bbd684135c70606465
Gaganjot singh <megaganjotsingh@gmail.com>
2023-11-20 22:32:09 +0530

    values updated for curved graph

--- ScrollableLineChart/ViewController.swift
+++ ScrollableLineChart/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        lineChart.isCurved  = true
         lineChart.dataEntries = [
             PointEntry(value: 1, label: "one"),
             PointEntry(value: 2, label: "two"),
